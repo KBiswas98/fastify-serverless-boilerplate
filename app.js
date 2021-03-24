3968a410bae9efc8c0d66d19646721c7567e623c
kbiswas98 <bkamalesh99@gmail.com>
2021-03-25 00:51:57 +0530

    update serverless.yml and add api_request

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const fastify = require('fastify')
+const fastify = require("fastify");
 
-const app = fastify()
-app.get('/', (request, reply) => reply.send({ hello: 'this is kamalesh' }))
-
+const app = fastify();
+app.get("/", (request, reply) => reply.send({ hello: "this is kamalesh" }));
 
 if (require.main === module) {
-  // called directly i.e. "node app"
   app.listen(9000, (err) => {
-    if (err) console.error(err)
-    console.log('server listening on 9000')
-  })
+    if (err) console.error(err);
+    console.log("server listening on 9000");
+  });
 } else {
-  // required as a module => executed on aws lambda
-  module.exports = app
+  module.exports = app;
 }
